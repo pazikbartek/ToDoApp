260067a7e2778f26d6ee7342e4416704692b96ff
bartek <bartek.pazik@interia.pl>
2019-04-08 13:01:09 +0200

    Updates

---
+++ precache-manifest.00ec61b97fd211b4cb00281fbb37c3e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c21fc1459e5e0452110e7f3dc5bfd18",
+    "url": "/ToDoApp/static/media/tlo.5c21fc14.jpg"
+  },
+  {
+    "revision": "fb1067834985d33aa2a1",
+    "url": "/ToDoApp/static/js/runtime~main.f2c45213.js"
+  },
+  {
+    "revision": "f8b3143d87b9432c2a69",
+    "url": "/ToDoApp/static/js/main.f1c5b72d.chunk.js"
+  },
+  {
+    "revision": "5117e5991e29a03144e1",
+    "url": "/ToDoApp/static/js/2.47d63b0b.chunk.js"
+  },
+  {
+    "revision": "f8b3143d87b9432c2a69",
+    "url": "/ToDoApp/static/css/main.fe721cb1.chunk.css"
+  },
+  {
+    "revision": "5117e5991e29a03144e1",
+    "url": "/ToDoApp/static/css/2.3c8c852a.chunk.css"
+  },
+  {
+    "revision": "4e3fb49351d907bdc3da470c3fdda392",
+    "url": "/ToDoApp/index.html"
+  }
+];
